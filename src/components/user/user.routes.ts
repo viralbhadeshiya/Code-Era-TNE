9a780bf60e07c71792c6d06e3b2d1a3a07b958ae
Viral Bhadeshiya <viralrbhadeshiya@gmail.com>
2022-04-06 07:58:20 +0530

    controller blueprint

--- src/components/user/user.routes.ts
+++ src/components/user/user.routes.ts
@@ -1,40 +1,39 @@
-// import { Router } from 'express';
+import { Router } from 'express';
 // import { authenticateMiddleware } from '../../middleware/authentication.middleware';
 // import { validateRequestMiddleware } from '../../middleware/error.middleware';
-// import UserController from './user.controller.js';
+import UserController from './user.controller';
 // import { signInUserSchema, signUpUserSchema } from './user.model.js';
 
 // /**
 //  * User route class
 //  */
-// class UsersRoute {
-//     path = '/users';
+class UsersRoute {
+    path = '/users';
 
-//     router = Router();
+    router = Router();
 
-//     userController = new UserController();
+    userController = new UserController();
 
-//     constructor() {
-//         this.initializeRoutes();
-//     }
+    constructor() {
+        this.initializeRoutes();
+    }
 
-//     initializeRoutes() {
-//         // No Auth router
-//         this.router.post(`${this.path}`, validateRequestMiddleware(signUpUserSchema), this.userController.signUpUser);
-//         this.router.post(
-//             `${this.path}/signIn`,
-//             validateRequestMiddleware(signInUserSchema),
-//             this.userController.signInUser,
-//         );
+    initializeRoutes() {
+        //         // No Auth router
+        //         this.router.post(`${this.path}`, validateRequestMiddleware(signUpUserSchema), this.userController.signUpUser);
+        //         this.router.post(
+        //             `${this.path}/signIn`,
+        //             validateRequestMiddleware(signInUserSchema),
+        //             this.userController.signInUser,
+        //         );
+        //         // Auth Router
+        //         this.router.get(`${this.path}/me`, authenticateMiddleware.authorize, this.userController.getUsers);
+        //         this.router.post(
+        //             `${this.path}/profile`,
+        //             authenticateMiddleware.authorize,
+        //             this.userController.updateProfilePicture,
+        //         );
+    }
+}
 
-//         // Auth Router
-//         this.router.get(`${this.path}/me`, authenticateMiddleware.authorize, this.userController.getUsers);
-//         this.router.post(
-//             `${this.path}/profile`,
-//             authenticateMiddleware.authorize,
-//             this.userController.updateProfilePicture,
-//         );
-//     }
-// }
-
-// export default UsersRoute;
+export default UsersRoute;
